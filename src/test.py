4d88e20bba32952c92bf284dd38ada03d0568b3d
PHANDAT-B <nphandat2006@gmail.com>
2026-08-23 12:34:35 +0700

    The first github actions project to test

---
+++ src/test.py
@@ -0,0 +1,15 @@
+def add(a,b):
+    return a+b
+def sub(a,b):
+    return a-b
+def multiple(a,b):
+    return a*b
+def div(a , b):
+    return a / b
+
+def test() :
+    assert add(1,2) == 3
+    assert sub(3,4) == -1
+    assert multiple(5,4) == 20
+    assert div(5,1) == 5
+ 
